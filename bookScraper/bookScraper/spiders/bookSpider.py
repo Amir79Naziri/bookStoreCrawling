3301690c34f118e1ec07425e720d8b2119268b69
Amir79Naziri <naziriamirreza@gmail.com>
2023-12-23 22:55:29 +0330

    at per book page crawling

--- bookScraper/bookScraper/spiders/bookSpider.py
+++ bookScraper/bookScraper/spiders/bookSpider.py
@@ -10,13 +10,14 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css("article.product_pod")
 
         for book in books:
-            yield {
-                "name": book.css("h3 a::text").get(),
-                "price": book.css(".product_price > p.price_color::text").get(),
-                "url": book.css("h3 a").attrib[
-                    "href"
-                ],  # book.css("h3 a::attr(href)").get(),
-            }
+            book_page = book.css("h3 a").attrib["href"]
+
+            if "catalogue/" in book_page:
+                book_url = "https://books.toscrape.com/" + book_page
+            else:
+                book_url = "https://books.toscrape.com/catalogue/" + book_page
+
+            yield response.follow(book_url, callback=self.book_parse)
 
         next_page = response.css("li.next a").attrib["href"]
 
@@ -27,3 +28,33 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = "https://books.toscrape.com/catalogue/" + next_page
 
             yield response.follow(next_page_url, callback=self.parse)
+
+    def book_parse(self, response):
+        table_rows = response.css("table.table.table-striped tr")
+
+        yield {
+            "url": response.url,
+            "title": response.xpath(
+                '//*[@id="content_inner"]/article/div[1]/div[2]/h1/text()'
+            )
+            .get()
+            .strip(),
+            "product_type": table_rows[1].css("td::text").get().strip(),
+            "price_excl_tax": table_rows[2].css("td::text").get().strip(),
+            "price_incl_tax": table_rows[3].css("td::text").get().strip(),
+            "tax": table_rows[4].css("td::text").get().strip(),
+            "availability": table_rows[5].css("td::text").get().strip(),
+            "num_reviews": table_rows[6].css("td::text").get().strip(),
+            "stars": response.css("p.star-rating").attrib["class"].strip(),
+            "category": response.xpath('//*[@id="default"]/div/div/ul/li[3]/a/text()')
+            .get()
+            .strip(),
+            "description": response.xpath('//*[@id="content_inner"]/article/p/text()')
+            .get()
+            .strip(),
+            "price": response.xpath(
+                '//*[@id="content_inner"]/article/div[1]/div[2]/p[1]/text()'
+            )
+            .get()
+            .strip(),
+        }
